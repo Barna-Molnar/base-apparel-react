5a0270e6ea94817a0a0a0270b92d361a774a936b
Barnabas Molnar <hanta911@gmail.com>
2021-06-19 23:35:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        We're coming soon Hello fellow shoppers! We're currently building our
+        new fashion store. Add your email below to stay up-to-date with
+        announcements and our launch deals. Email Address
+        <footer>
+          <p class="attribution">
+            Challenge by{' '}
+            <a href="https://www.frontendmentor.io?ref=challenge">
+              Frontend Mentor
+            </a>
+            . Coded by <a href="#">Your Name Here</a>.
+          </p>
+        </footer>
+      </header>
+    </div>
+  );
+}
+
+export default App;
